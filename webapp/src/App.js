1235e3f3eb1ae97bcedddeccc8ac4cc5e8aef55a
Krzysztof Antoniak <kantoniak@kantoniak.com>
2021-09-25 00:32:31 +0200

    Add router

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,27 +1,64 @@
 import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload 3.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/a">A</Link>
+            </li>
+            <li>
+              <Link to="/b">B</Link>
+            </li>
+          </ul>
+        </nav>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Switch>
+            <Route path="/a">
+              <PageA />
+            </Route>
+            <Route path="/b">
+              <PageB />
+            </Route>
+            <Route path="/">
+              <PageHome />
+            </Route>
+          </Switch>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Router>
   );
 
 
+}
+
+function PageA() {
+  return <p>A</p>;
+}
+
+function PageB() {
+  return <p>B</p>;
+}
+
+function PageHome() {
+  return <p>Home</p>;
 }
 
 export default App;
