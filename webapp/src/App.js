32d7052409a344ff47ee1a470e355cf73537204e
Yi Zu <zuyi@bu.edu>
2021-09-25 20:46:59 +0200

    Switch App.js to functional component

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,36 +1,31 @@
-import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import DetailsPage from './pages/DetailsPage';
 import SwipePage from './pages/SwipePage';
 
-class App extends React.Component {
+const App = () => {
+  const [appTitle] = useState('ARTSEE');
 
-  constructor() {
-    super();
-    this.state = {
-      appTitle: "SuperApp"
-    };
-  }
-
-  render() {
-    return (
-      <Router>
-        <div className="container-fluid vh-100 d-flex flex-column">
-          <section className="row py-3 text-center" style={{ background: '#EEE' }}>
-            <h1 className="col mb-0">{this.state.appTitle}</h1>
-          </section>
-          <Switch>
-            <Route path="/details">
-              <DetailsPage />
-            </Route>
-            <Route path="/">
-              <SwipePage />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
+  return (
+    <Router>
+      <div className='container-fluid vh-100 d-flex flex-column'>
+        <section
+          className='row py-3 text-center'
+          style={{ background: '#EEE' }}
+        >
+          <h1 className='col mb-0'>{appTitle}</h1>
+        </section>
+        <Switch>
+          <Route path='/details'>
+            <DetailsPage />
+          </Route>
+          <Route path='/'>
+            <SwipePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
