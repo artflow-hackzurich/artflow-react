f69f459a4c26a45ab6eeb355dd4e397bc3f8947f
Krzysztof Antoniak <kantoniak@kantoniak.com>
2021-09-25 06:53:54 +0200

    Handle swiping from the deck

--- webapp/src/components/ArtworkCard.js
+++ webapp/src/components/ArtworkCard.js
@@ -3,43 +3,61 @@ import { motion, useAnimation, useMotionValue, useTransform } from 'framer-motio
 import './ArtworkCard.css';
 
 function ArtworkCard({ title, desc, imageUrl }) {
-    const windowWidth = document.documentElement.clientWidth;
-    console.log(windowWidth);
-    const x = useMotionValue(0);
-    const rotate = useTransform(x, [-0.6 * windowWidth, 0.6 * windowWidth], [-30, 30]);
-    const opacity = useTransform(x, [-0.6 * windowWidth, -0.2 * windowWidth, 0, 0.2 * windowWidth, 0.6 * windowWidth], [0, 1, 1, 1, 0]);
-    const animControls = useAnimation();
-
-    const onDragEndHandler = (e, info) => {
-      const windowWidth = document.documentElement.clientWidth;
-      if (Math.abs(info.offset.x) <= 0.4 * windowWidth) {
-        animControls.start({ x: 0 });
-      } else {
-        const isLeft = (info.offset.x < 0);
-        const swipeEvent = new CustomEvent("swipe", {
-          detail: {
-            dir: (isLeft ? "left" : "right")
-          }
-        })
-        animControls.start({ x: isLeft ? -windowWidth : windowWidth, opacity: 0 });
-        window.dispatchEvent(swipeEvent);
-      }
-    };
-
     return (
-      <motion.div
-          animate={animControls}
-          drag="x"
-          dragConstraints={{ left: -windowWidth, right: windowWidth }}
+      <div
           className="artwork-card col card flex-grow-1 d-flex flex-column justify-content-end"
-          style={{ x, rotate, opacity, backgroundImage: 'url(' + imageUrl + ')' }}
-          onDragEnd={onDragEndHandler}>
+          style={{ backgroundImage: 'url(' + imageUrl + ')' }}>
         <div className="card-body flex-grow-0 p-4 pt-5">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{desc}</p>
         </div>
-      </motion.div>
+      </div>
     );
 }
 
+function DraggableArtworkCard({ title, desc, imageUrl }) {
+  const windowWidth = document.documentElement.clientWidth;
+  const x = useMotionValue(0);
+  const rotate = useTransform(x, [-0.6 * windowWidth, 0.6 * windowWidth], [-30, 30]);
+  const opacity = useTransform(x, [-0.6 * windowWidth, -0.2 * windowWidth, 0, 0.2 * windowWidth, 0.6 * windowWidth], [0, 1, 1, 1, 0]);
+  const animControls = useAnimation();
+
+  const onDragEndHandler = (e, info) => {
+    const windowWidth = document.documentElement.clientWidth;
+    if (Math.abs(info.offset.x) <= 0.4 * windowWidth) {
+      animControls.start({ x: 0 });
+    } else {
+      const isLeft = (info.offset.x < 0);
+      const swipeEvent = new CustomEvent("swipe", {
+        detail: {
+          dir: (isLeft ? "left" : "right")
+        }
+      })
+      const goAwayPromise = async () => {
+        await animControls.start({ x: isLeft ? -windowWidth : windowWidth, opacity: 0 });
+      }
+      goAwayPromise().then(() => {
+        x.set(0);
+        window.dispatchEvent(swipeEvent);
+      });
+    }
+  };
+
+  return (
+    <motion.div
+        animate={animControls}
+        drag="x"
+        dragConstraints={{ left: -windowWidth, right: windowWidth }}
+        className="artwork-card col card flex-grow-1 d-flex flex-column justify-content-end"
+        style={{ x, rotate, opacity, backgroundImage: 'url(' + imageUrl + ')' }}
+        onDragEnd={onDragEndHandler}>
+      <div className="card-body flex-grow-0 p-4 pt-5">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{desc}</p>
+      </div>
+    </motion.div>
+  );
+}
+
+export { DraggableArtworkCard };
 export default ArtworkCard;
--- webapp/src/components/SwipeArea.js
+++ webapp/src/components/SwipeArea.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import ArtworkCard from '../components/ArtworkCard';
+import ArtworkCard, { DraggableArtworkCard } from '../components/ArtworkCard';
+import './SwipeArea.css';
 
 class SwipeArea extends React.Component {
 
   constructor(props){
     super(props);
+    this.state = {
+      artworks: [
+        { name: 'Starry Night', desc: 'A great classic', imageUrl: 'http://172.22.0.2:3000/starry-night.jpg' },
+        { name: 'Mona Lisa', desc: 'Another one', imageUrl: 'http://172.22.0.2:3000/mona-lisa.jpg' },
+        { name: 'Starry Night', desc: 'A great classic', imageUrl: 'http://172.22.0.2:3000/starry-night.jpg' },
+        { name: 'Mona Lisa', desc: 'Another one', imageUrl: 'http://172.22.0.2:3000/mona-lisa.jpg' }
+      ]
+    }
     this.onSwipe = this.onSwipe.bind(this);
   }
 
@@ -17,14 +26,19 @@ class SwipeArea extends React.Component {
   }
 
   onSwipe(e) {
-    alert("swipe " + e.detail.dir + "!");
+    console.log("Swipe " + e.detail.dir);
+    var newArtworks = [...this.state.artworks];
+    newArtworks.shift();
+    this.setState({ artworks: newArtworks });
   }
 
   render() {
+    const front = this.state.artworks[0];
+    const back = this.state.artworks[1];
     return (
       <div className="col flex-grow-1 d-flex p-4">
-        <ArtworkCard title="Starry Night" desc="A great classic" imageUrl="http://172.22.0.2:3000/starry-night.jpg" />
-        <ArtworkCard title="Starry Night" desc="A great classic" imageUrl="http://172.22.0.2:3000/starry-night.jpg" />
+        {this.state.artworks.length >= 1 && <DraggableArtworkCard title={front.name} desc={front.desc} imageUrl={front.imageUrl} />}
+        {this.state.artworks.length >= 2 && <ArtworkCard title={back.name} desc={back.desc} imageUrl={back.imageUrl} />}
       </div>
     );
   }
